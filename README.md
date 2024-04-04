9e5edbdd23a1cddce7fcc2070ab333ad26f62ae4
Yes2eul <yeseul5611@gmail.com>
2024-04-04 17:35:51 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+### Movieing Box
+
+영화진흥위원회 Open Api를 활용한 애플리케이션
+
+- React, JavaScript, Next.js, Styled-Components, Axios
---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+      <header>Movieing Box</header>
+      <div>Box Office</div>
+      <div>
+        <div>Daily</div>
+        <div>Weekly</div>
+      </div>
+      <div>Search Movie</div>
+      <div>Movie List</div>
+      <div>Movie Info</div>
+    </>
+  );
+}
